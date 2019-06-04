345dd7f449842c00e6dc6e6bf0c18f1ba91ba9b8
markewilson <mark.e.wilson@gmail.com>
2019-06-04 14:42:18 -0400

    adds variables and markup to random course block

--- web/modules/custom/as_courses/src/Plugin/Block/RandomBlock.php
+++ web/modules/custom/as_courses/src/Plugin/Block/RandomBlock.php
@@ -18,9 +18,62 @@ class RandomBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $build = [];
-    $build['random_block']['#markup'] = 'Implement RandomBlock.';
 
+$config = $this->getConfiguration();
+    //kint($config);
+    if (!empty($config['semester'])) {
+      $semester = $config['semester'];
+    }
+    else {
+      $semester = "FA19";
+    }
+    if (!empty($config['semestername'])) {
+      $semestername = $config['semestername'];
+    }
+    else {
+      $semestername = "Fall 2019";
+    }
+    if (!empty($config['courses_shown'])) {
+      $courses_shown = $config['courses_shown'];
+    }
+    else {
+      //1 shows 2, 2 shows 3 etc.
+      $courses_shown = 1;
+    }
+
+    if (!empty($config['keyword_params'])) {
+      $keyword_params = $config['keyword_params'];
+    }
+    else {
+      $keyword_params = "MATH";
+    }
+    if (!empty($config['major_name'])) {
+      $major_name = $config['major_name']['#markup'];
+    }
+    else {
+      $major_name = "Major Name";
+    }
+    $build = [];
+    $build['random_block']['#markup'] = "";
+    $course_count = 0;
+    $course_json = as_courses_get_courses_json($semester,$keyword_params);
+    if (!empty($course_json)) {
+      foreach($course_json as $course_data) {
+        if ($course_count <= $courses_shown) {
+            $build['random_block']['#markup'] = $build['random_block']['#markup'] . as_courses_generate_course_item_markup($course_data,$semester);
+          $course_count++;
+        }
+
+      }
+      $build['random_block']['#markup'] = $build['random_block']['#markup'] . "<div><a href='https://classes.cornell.edu/browse/roster/" . $semester . "/subject/" . $keyword_params . "'>Full Listing of " . $major_name . " courses for " . $semestername . " Semester</a></div>";
+    } // There were no courses
+    else {
+      $build['random_block']['#markup'] = "<main>
+                <h1>Courses</h1>
+                <p>There are no courses to list.</p>
+                </main>";
+    }
+    //$build['random_block']['#markup'] = 'Implement RandomBlock.';
     return $build;
   }
 
