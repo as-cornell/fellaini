4f9b15b83f4a74858c161e5f1f9fef8a559e0275
markewilson <mark.e.wilson@gmail.com>
2019-06-05 12:27:11 -0400

    adds instagram block template and updates as_instagram custom module to woulk with Instagram Block page component

--- web/modules/custom/as_instagram/src/Plugin/Block/DefaultBlock.php
+++ web/modules/custom/as_instagram/src/Plugin/Block/DefaultBlock.php
@@ -9,61 +9,52 @@ use Drupal\Core\Form\FormStateInterface;
  * Provides a 'DefaultBlock' block.
  *
  * @Block(
- *  id = "default_block",
+ *  id = "instagram_block",
  *  admin_label = @Translation("Instagram block"),
  * )
  */
 class DefaultBlock extends BlockBase {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function defaultConfiguration() {
-    return [
-            'instagram_account_name' => cornellcas,
-                  'posts' => 1,
-          ] + parent::defaultConfiguration();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockForm($form, FormStateInterface $form_state) {
-    $form['instagram_account_name'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Instagram Account Name'),
-    '#description' => $this->t('The username of the instagram account to be featured'),
-      '#default_value' => $this->configuration['instagram_account_name'],
-      '#maxlength' => 64,
-      '#size' => 64,
-      '#weight' => '0',
-    ];
-    $form['posts'] = [
-      '#type' => 'number',
-      '#title' => $this->t('Posts'),
-    '#description' => $this->t('Number of posts to display'),
-      '#default_value' => $this->configuration['posts'],
-      '#weight' => '0',
-    ];
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockSubmit($form, FormStateInterface $form_state) {
-    $this->configuration['instagram_account_name'] = $form_state->getValue('instagram_account_name');
-    $this->configuration['posts'] = $form_state->getValue('posts');
-  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
     $build = [];
-    $build['default_block_instagram_account_name']['#markup'] = '<p>' . $this->configuration['instagram_account_name'] . '</p>';
-    $build['default_block_posts']['#markup'] = '<p>' . $this->configuration['posts'] . '</p>';
+    $build['instagram_block']['#markup'] = "";
+    $config = $this->getConfiguration();
+    //kint($config);
+    if (!empty($config['posts_shown'])) {
+      $posts_shown = $config['posts_shown'];
+    }
+    else {
+      //1 shows 2, 2 shows 3 etc.
+      $posts_shown = 0;
+    }
+
+    if (!empty($config['keyword_params'])) {
+      $keyword_params = $config['keyword_params'];
+    }
+    else {
+      $keyword_params = "cornellcas";
+    }
+
+    $post_count = 0;
+    $instagram_json = as_instagram_get_instagram_json($keyword_params);
+    dump ($instagram_json);
+    if (!empty($instagram_json)) {
+      foreach($instagram_json as $post_data) {
+        if ($post_count <= $posts_shown) {
+            $build['instagram_block']['#markup'] = $build['instagram_block']['#markup'] . as_instagram_generate_instagram_post_markup($post_data);
+          $post_count++;
+        }
+      }
+    } // There were no instagram posts
+    else {
+      $build['instagram_block']['#markup'] = "<main>
+                <p>There are no Instagram posts to list.</p>
+                </main>";
+    }
 
     return $build;
   }
