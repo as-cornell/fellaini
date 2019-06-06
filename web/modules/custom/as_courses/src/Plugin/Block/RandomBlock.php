fbaa5d531eee47c5f730693ccebeced4818a4233
markewilson <mark.e.wilson@gmail.com>
2019-06-06 15:00:52 -0400

    updates methos for courses/events/posts shown to use real numbers instead of n-1, updates random courses block to use shuffle() to pick random set of courses to display

--- web/modules/custom/as_courses/src/Plugin/Block/RandomBlock.php
+++ web/modules/custom/as_courses/src/Plugin/Block/RandomBlock.php
@@ -33,14 +33,13 @@ $config = $this->getConfiguration();
     else {
       $semestername = "Fall 2019";
     }
-    //if (!empty($config['courses_shown'])) {
-      //$courses_shown = $config['courses_shown'];
-    //}
-    //else {
-      //1 shows 2, 2 shows 3 etc.
-      //$courses_shown = 1;
-    //}
-
+    if (!empty($config['courses_shown'])) {
+      //1 shows 2, 2 shows 3 etc. so subtract 1
+      $courses_shown = $config['courses_shown'] - 1;
+    }
+    else {
+      $courses_shown = 0;
+    }
     if (!empty($config['keyword_params'])) {
       $keyword_params = $config['keyword_params'];
     }
@@ -55,14 +54,22 @@ $config = $this->getConfiguration();
     }
     $build = [];
     $build['random_block']['#markup'] = "";
-    //$course_count = 0;
+    $course_count = 0;
     $course_json = as_courses_get_courses_json($semester,$keyword_params);
-    // https://stackoverflow.com/questions/31566377/randomly-select-item-from-json-in-php
     if (!empty($course_json)) {
-      $course_data = ($course_json[rand(0, count($course_json) - 1)]);
-      //dump ($course_data);
-      $build['random_block']['#markup'] = $build['random_block']['#markup'] . as_courses_generate_course_item_markup($course_data,$semester);
-
+      //one random course with rand()
+      // https://stackoverflow.com/questions/31566377/randomly-select-item-from-json-in-php
+      //$course_data = ($course_json[rand(0, count($course_json) - 1)]);
+      //$build['random_block']['#markup'] = $build['random_block']['#markup'] . as_courses_generate_course_item_markup($course_data,$semester);
+      //multiple random courses with shuffle()
+      //https://www.w3schools.com/php/func_array_shuffle.asp
+      shuffle($course_json);
+      foreach($course_json as $course_data) {
+        if ($course_count <= $courses_shown) {
+            $build['random_block']['#markup'] = $build['random_block']['#markup'] . as_courses_generate_course_item_markup($course_data,$semester);
+          $course_count++;
+        }
+      }
       $build['random_block']['#markup'] = $build['random_block']['#markup'] . "<div><a href='https://classes.cornell.edu/browse/roster/" . $semester . "/subject/" . $keyword_params . "'>Full Listing of " . $major_name . " courses for " . $semestername . " Semester</a></div>";
     } // There were no courses
     else {
@@ -74,5 +81,4 @@ $config = $this->getConfiguration();
     //$build['random_block']['#markup'] = 'Implement RandomBlock.';
     return $build;
   }
-
 }
