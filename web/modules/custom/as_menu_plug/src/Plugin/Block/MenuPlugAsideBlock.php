0c1372416e76b669352963823684f1a0ddb4497c
markewilson <mark.e.wilson@gmail.com>
2020-03-18 14:45:31 -0400

    adds blocks generated via drupal console for Aside and Card menu plug links

---
+++ web/modules/custom/as_menu_plug/src/Plugin/Block/MenuPlugAsideBlock.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\as_menu_plug\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'MenuPlugAsideBlock' block.
+ *
+ * @Block(
+ *  id = "menu_plug_aside_block",
+ *  admin_label = @Translation("Menu plug aside block"),
+ * )
+ */
+class MenuPlugAsideBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['entity_reference'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => $this->t('Entity Reference'),
+      '#description' => $this->t('Entity Reference'),
+      '#default_value' => $this->configuration['entity_reference'],
+      '#weight' => '0',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['entity_reference'] = $form_state->getValue('entity_reference');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['#theme'] = 'menu_plug_aside_block';
+    $build['#conten'][] = $this->configuration['entity_reference'];
+
+    return $build;
+  }
+
+}
