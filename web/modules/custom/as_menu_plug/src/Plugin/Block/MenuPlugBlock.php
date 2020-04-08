f6c515c1c986fdb1445c7915e3a4f9715a5c756c
markewilson <mark.e.wilson@gmail.com>
2020-04-08 16:28:31 -0400

    further menu cleanup and extending li and ul classes to menu plug block

--- web/modules/custom/as_menu_plug/src/Plugin/Block/MenuPlugBlock.php
+++ web/modules/custom/as_menu_plug/src/Plugin/Block/MenuPlugBlock.php
@@ -24,15 +24,23 @@ class MenuPlugBlock extends BlockBase {
     $build['menu_plug_block']['#markup'] = "";
     $menu_link_id = "";
     $menu_children = "";
+    $menu_level = "";
+    $link_class = "";
+    $list_class = "";
     $config = $this->getConfiguration();
     if (!empty($config['menu_link_id'])) {
       $menu_link_id = $config['menu_link_id'];
     }
+    if (!empty($config['menu_level'])) {
+      $menu_level = $config['menu_level'];
+    }
     if (!empty($config['menu_children'])) {
       $menu_children = $config['menu_children'];
     }
-    // strip $menu_link_id
-    //$menu_link_id = as_menu_plug_strip_id($menu_link_id);
+    // get link class
+    $link_class = as_menu_plug_generate_link_class($menu_level,$menu_children);
+    // get link class
+    $list_class = as_menu_plug_generate_list_class($menu_level);
     // get $nid from $menu_link_id uri
     $nid = as_menu_plug_menulinkid_nid($menu_link_id);
     // strip $nid
@@ -50,10 +58,7 @@ class MenuPlugBlock extends BlockBase {
     if (!empty($node->field_landing_page_component_ent)) {
     $flpce = $node->field_landing_page_component_ent;
       }
-
-    // build markup
-    $build['menu_plug_block']['#markup'] = $build['menu_plug_block']['#markup'] . '<ul>';
-
+    $build['menu_plug_block']['#markup'] = $build['menu_plug_block']['#markup'] . "<ul class = '".$list_class."''>";
     // get page component entities
     if (!empty($fpce)) {
       $index = 0;
@@ -63,10 +68,13 @@ class MenuPlugBlock extends BlockBase {
             // this uses only field_page_section_title from a page section entity
             if (!empty($node->field_page_components_entity[$index]->entity->field_page_section_title[0])) {
             $link_title = $node->field_page_components_entity[$index]->entity->field_page_section_title[0]->value;
-            $build['menu_plug_block']['#markup'] = $build['menu_plug_block']['#markup'] . as_menu_plug_generate_link_markup($link_title,$alias);
+            $build['menu_plug_block']['#markup'] = $build['menu_plug_block']['#markup'] . as_menu_plug_generate_link_markup($link_title,$alias,$link_class);
                }
         $index++;
         }
+        //if (empty($menu_children)){
+        $build['menu_plug_block']['#markup'] = $build['menu_plug_block']['#markup'] . "</ul>";
+        //}
       }
     // get landing page component entities
     if (!empty($flpce)) {
@@ -77,14 +85,14 @@ class MenuPlugBlock extends BlockBase {
           // this uses only field_page_section_title from a page section entity
             if (!empty($node->field_landing_page_component_ent[$index]->entity->field_page_section_title[0])) {
           $link_title = $node->field_landing_page_component_ent[$index]->entity->field_page_section_title[0]->value;
-          $build['menu_plug_block']['#markup'] = $build['menu_plug_block']['#markup'] . as_menu_plug_generate_link_markup($link_title,$alias);
+          $build['menu_plug_block']['#markup'] = $build['menu_plug_block']['#markup'] . as_menu_plug_generate_link_markup($link_title,$alias,$link_class);
           }
       $index++;
       }
+      //if (empty($menu_children)){
+      $build['menu_plug_block']['#markup'] = $build['menu_plug_block']['#markup'] . "</ul>";
+      //}
     }
-    if ($menu_children != TRUE){
-    $build['menu_plug_block']['#markup'] = $build['menu_plug_block']['#markup'] .'</ul>';
-   }
     return $build;
   }
 }
