ec1cdb955eb1d59d5d5b82dc07bd6310b12b8685
markewilson <mark.e.wilson@gmail.com>
2020-06-02 10:45:31 -0400

    adds taxonomy term page templates for article bylines and media sources

--- pulldown.sh
+++ pulldown.sh
@@ -3,5 +3,6 @@ platform db:dump --relationship first -y
 drush @drupalvm.fellaini sql-drop -y
 drush @drupalvm.fellaini sql-cli < ydfzxlmxa2d3a--master-7rqtwti--mysqldb--firstdb--dump.sql
 drush @drupalvm.fellaini pmu simplesamlphp_auth
+drush @drupalvm.fellaini en twig_vardumper
 drush @drupalvm.fellaini cr
 drush @drupalvm.fellaini uli
