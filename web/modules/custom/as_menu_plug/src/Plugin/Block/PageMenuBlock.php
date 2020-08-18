da11f9a046d8d8960e107caa7ab2bb9303a503ec
bobmarteal <bobmarteal@gmail.com>
2020-08-18 12:25:28 -0400

    lots of aside work

--- web/modules/custom/as_menu_plug/src/Plugin/Block/PageMenuBlock.php
+++ web/modules/custom/as_menu_plug/src/Plugin/Block/PageMenuBlock.php
@@ -6,21 +6,23 @@ use Drupal\Core\Block\BlockBase;
 
 
 /**
- * Provides a 'PageMenuBlock' block.
+ * Provides a 'PageMenuBlock' block speicifically for asideNav internal page items
  *
  * @Block(
  *  id = "page_menu_block",
  *  admin_label = @Translation("Page menu block"),
  * )
  */
-class PageMenuBlock extends BlockBase {
+class PageMenuBlock extends BlockBase
+{
 
 
 
   /**
    * {@inheritdoc}
    */
-  public function build() {
+  public function build()
+  {
 
     $link_class = 'asideNav__item';
     $list_class = 'asideNav';
@@ -33,20 +35,18 @@ class PageMenuBlock extends BlockBase {
       $link_values = $config['link_values'];
     }
 
-    $build['page_menu_block']['#markup'] = '<ul class="'.$list_class.'">';
+    $build['page_menu_block']['#markup'] = '<ul class="' . $list_class . '">';
 
     if (!empty($link_values)) {
-      foreach($link_values as $link_title) {
-            $build['page_menu_block']['#markup'] = $build['page_menu_block']['#markup'] . as_menu_plug_generate_link_markup($link_title,$alias,$link_class);
+      foreach ($link_values as $link_title) {
+        $build['page_menu_block']['#markup'] = $build['page_menu_block']['#markup'] . as_menu_plug_generate_link_markup($link_title, $alias, $link_class);
       }
-
     } // There were no links
     else {
-      $build['page_menu_block']['#markup'] = $build['page_menu_block']['#markup'] ."<li>There are no in-page links</li>";
+      $build['page_menu_block']['#markup'] = $build['page_menu_block']['#markup'] . "<li>There are no in-page links</li>";
     }
-        $build['page_menu_block']['#markup'] = $build['page_menu_block']['#markup'] .'</ul>';
+    $build['page_menu_block']['#markup'] = $build['page_menu_block']['#markup'] . '</ul>';
 
     return $build;
   }
-
 }
