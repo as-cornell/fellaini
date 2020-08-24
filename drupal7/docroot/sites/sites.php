19711f3781364421076228aef8322f32cda03d87
markewilson <mark.e.wilson@gmail.com>
2020-08-24 09:27:10 -0400

    updates sites.php with new local uris for drupal 7 sites

--- drupal7/docroot/sites/sites.php
+++ drupal7/docroot/sites/sites.php
@@ -1,8 +1,8 @@
 <?php
 // DRUPAL-VM
-$sites['cantona.fellaini.local'] = 'default';
-$sites['scholsie.dept1.fellaini.local'] = 'dept1';
-$sites['scholsie.dept2.fellaini.local'] = 'dept2';
+$sites['scholsie.fellaini.local'] = 'default';
+$sites['dept1.scholsie.fellaini.local'] = 'dept1';
+$sites['dept2.scholsie.fellaini.local'] = 'dept2';
 // PLATFORM
 $sites['asd7.as.cornell.edu'] = 'default';
 $sites['dept1.as.cornell.edu'] = 'dept1';
