c3815cddb03a67480b0209cbc61cb03f06b6eea5
markewilson <mark.e.wilson@gmail.com>
2020-09-09 14:44:31 -0400

    config update

--- drupal8/pulldown.sh
+++ drupal8/pulldown.sh
@@ -1,5 +1,5 @@
-platform mount:download --mount=web/sites/default/files --target=web/sites/default/files -y
-platform db:dump --relationship first -y
+platform mount:download --app=drupal8 --mount=web/sites/default/files --target=web/sites/default/files -y
+platform db:dump --app=drupal8 --relationship first -y
 drush @drupalvm.fellaini sql-drop -y
 drush @drupalvm.fellaini sql-cli < ../ydfzxlmxa2d3a--master-7rqtwti--mysqldb--firstdb--dump.sql
 drush @drupalvm.fellaini pmu simplesamlphp_auth
