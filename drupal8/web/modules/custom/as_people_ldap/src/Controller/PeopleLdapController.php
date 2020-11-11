4649c7795971b90b6137d16678bc4c87853f45eb
markewilson <mark.e.wilson@gmail.com>
2020-11-11 17:25:54 -0500

    adds people ldap block and adds to person template, enablesldap extension

--- drupal8/web/modules/custom/as_people_ldap/src/Controller/PeopleLdapController.php
+++ drupal8/web/modules/custom/as_people_ldap/src/Controller/PeopleLdapController.php
@@ -3,9 +3,6 @@
 namespace Drupal\as_people_ldap\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\ldap_servers\LdapBridgeInterface;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\Ldap\Exception\LdapException;
 
 
 class PeopleLdapController extends ControllerBase {
@@ -17,8 +14,10 @@ class PeopleLdapController extends ControllerBase {
    */
   public function content($netid) {
     //$main = $netid;
-    $main = as_people_ldap_get_netid_ldap($netid);
-
+    $info= as_people_ldap_get_netid_ldap($netid);
+    $main = $info[0]["cornelleducampusaddress"][0] .'<br />';
+    $main = $main. '<a href="mailto:'.$info[0]["mail"][0].'">'.$info[0]["mail"][0].'</a><br />';
+    $main = $main. $info[0]["cornelleducampusphone"][0].'<br />';
     return array(
       '#type' => 'markup',
       '#markup' => $this->t('<h1>LDAP Data for '.$netid.'</h1><div class="slides">
