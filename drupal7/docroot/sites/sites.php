439318d3377815841ec6d2ff5c4d1eabe41d0a9a
markewilson <mark.e.wilson@gmail.com>
2020-12-09 15:22:24 -0500

    updates drupal7 sites.php with next 5 domains

--- drupal7/docroot/sites/sites.php
+++ drupal7/docroot/sites/sites.php
@@ -5,11 +5,11 @@ $sites['dept1.fellaini.local'] = 'dept1';
 $sites['dept2.fellaini.local'] = 'dept2';
 // PLATFORM
 $sites['asd7.as.cornell.edu'] = 'default';
-$sites['dept1.as.cornell.edu'] = 'organ';
-$sites['dept2.as.cornell.edu'] = 'cogsci';
-$sites['dept3.as.cornell.edu'] = 'cder';
-$sites['dept4.as.cornell.edu'] = 'frenchstudies';
-$sites['dept5.as.cornell.edu'] = 'igcs';
+$sites['dept1.as.cornell.edu'] = 'sagan';
+$sites['dept2.as.cornell.edu'] = 'africana';
+$sites['dept3.as.cornell.edu'] = 'americanstudies';
+$sites['dept4.as.cornell.edu'] = 'anthro';
+$sites['dept5.as.cornell.edu'] = 'archaeology';
 $sites['dept6.as.cornell.edu'] = 'mediastudies';
 $sites['dept7.as.cornell.edu'] = 'medieval';
 $sites['dept8.as.cornell.edu'] = 'rural';
@@ -25,3 +25,9 @@ $sites['medievalstudies.cornell.edu'] = 'medieval';
 $sites['rural.as.cornell.edu'] = 'rural';
 $sites['sct.cornell.edu'] = 'sct';
 $sites['icm.as.cornell.edu'] = 'icm';
+$sites['carlsaganinstitute.cornell.edu'] = 'sagan';
+$sites['africana.cornell.edu'] = 'africana';
+$sites['americanstudies.cornell.edu'] = 'americanstudies';
+$sites['anthropology.cornell.edu'] = 'anthro';
+$sites['archaeology.cornell.edu'] = 'archaeology';
+
