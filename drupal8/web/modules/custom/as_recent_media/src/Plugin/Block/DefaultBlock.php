4b56f635772671b56efc353bbc5c58a8dfd9597a
markewilson <mark.e.wilson@gmail.com>
2021-01-05 17:07:53 -0500

    adds modules/custom/as_recent_media/

---
+++ drupal8/web/modules/custom/as_recent_media/src/Plugin/Block/DefaultBlock.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\as_recent_media\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'DefaultBlock' block.
+ *
+ * @Block(
+ *  id = "recent_media_block",
+ *  admin_label = @Translation("Recent Media"),
+ * )
+ */
+class DefaultBlock extends BlockBase {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['recent_media_block']['#markup'] = "";
+    $config = $this->getConfiguration();
+    //kint($config);
+    if (!empty($config['items_shown'])) {
+      //1 shows 2, 2 shows 3 etc. so subtract 1
+      $items_shown = $config['items_shown'] - 1;
+    }
+    else {
+      $items_shown = 0;
+    }
+
+    $items_count = 0;
+    $recent_media_json = as_recent_media_get_json();
+    //dump ($recent_media_json);
+    if (!empty($recent_media_json)) {
+      foreach($recent_media_json as $media_data) {
+        if ($items_count <= $items_shown) {
+            $build['recent_media_block']['#markup'] = $build['recent_media_block']['#markup'] . as_recent_media_generate_item_markup($media_data);
+          $items_count++;
+        }
+      }
+    } // There were no instagram posts
+    else {
+      $build['recent_media_block']['#markup'] = "<main>
+                <p>There are no recent media items to list.</p>
+                </main>";
+    }
+
+    return $build;
+  }
+
+}
