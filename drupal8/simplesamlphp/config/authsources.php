5919a18ddb0d4a5ebc8e16eaceecfbcc39605181
markewilson <mark.e.wilson@gmail.com>
2021-01-22 13:04:38 -0500

    updates saml authsources to move as to drupal8

--- drupal8/simplesamlphp/config/authsources.php
+++ drupal8/simplesamlphp/config/authsources.php
@@ -17,6 +17,13 @@ $config = array(
         'entityID' => 'https://asd8.as.cornell.edu/',
         'idp' => 'https://shibidp-test.cit.cornell.edu/idp/shibboleth'
     ),
+    'as-sp' => array(
+        'saml:SP',
+        'privatekey' => 'prod.pem',
+        'certificate' => 'prod.crt',
+        'entityID' => 'https://as.cornell.edu/',
+        'idp' => 'https://shibidp.cit.cornell.edu/idp/shibboleth',
+    ),
     'people-sp' => array(
         'saml:SP',
         'privatekey' => 'saml.pem',
