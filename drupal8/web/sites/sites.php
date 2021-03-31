bf1f7d3164957421f22104a39372acd63ffea792
markewilson <mark.e.wilson@gmail.com>
2021-03-31 14:12:47 -0400

    updates sites.php in drupal8 to be ready for people.as switch

--- drupal8/web/sites/sites.php
+++ drupal8/web/sites/sites.php
@@ -85,5 +85,5 @@ if (!empty($_ENV['PLATFORM_ROUTES'])) {
 
 $sites['asd8.as.cornell.edu'] = 'default';
 $sites['as.cornell.edu'] = 'default';
-$sites['departments.asd8.as.cornell.edu'] = 'departments';
 $sites['people.asd8.as.cornell.edu'] = 'people';
+$sites['people.as.cornell.edu'] = 'people';
